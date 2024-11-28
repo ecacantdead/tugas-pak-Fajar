5b25409644989ee341223ad0cb5fd88c053bf871
ecacantdead <ecamad101008@gmail.com>
2024-11-29 07:06:21 +0800

    Add files via upload

---
+++ latihan1.js
@@ -0,0 +1,4 @@
+console.log("hello arif eni");
+alert("Acccept Cookies");
+var hasil = document.getElementById("hasil-output");
+hasil.innerHTML = " <h1> Biodata </h1> <p>Nama : Reza Ahmad Prawira Arifin </p> <p>kelas : XI PPLG</p>";
